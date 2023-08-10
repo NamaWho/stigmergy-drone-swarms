bb9b5a8ef59edad3e6fbe270fe50ca386e7ebd8d
Daniel Namaki <namaki.work@gmail.com>
2023-08-10 13:07:54 +0200

    fix: heatmap visualization

--- utils/stigmergy/heatmap.py
+++ utils/stigmergy/heatmap.py
@@ -39,12 +39,12 @@ def show_heatmap(field: List[List[Patch]]) -> None:
     for i, row in enumerate(field):
         for j, patch in enumerate(row):
             heatmap_data[i, j] = sum(pheromone.get_intensity for pheromone in patch.get_pheromones())
-
-    heatmap_data = heatmap_data.T
+    
+    # heatmap_data = heatmap_data.T
 
     plt.clf()
     plot = plt.imshow(heatmap_data, cmap='YlOrRd', interpolation='nearest')
-    plt.colorbar(plot, label="Pheromone Heat")
+    plt.colorbar(plot, label="Patch Pheromones Intensity")
     plt.grid(visible=True, which='both', linestyle='-', linewidth=1, color='black')
     plt.xticks(range(len(field[0])), range(len(field[0])))
     plt.yticks(range(len(field)), range(len(field)))
