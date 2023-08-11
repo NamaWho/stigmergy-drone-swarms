dc498753b769f4abe402afc89f08ea98121f0112
Daniel Namaki <namaki.work@gmail.com>
2023-08-11 17:13:34 +0200

    docs: squareperimeter

--- utils/stigmergy/squareperimeter.py
+++ utils/stigmergy/squareperimeter.py
@@ -1,6 +1,10 @@
 from math import floor
 
 def calculate_square_boundaries(drone_position_x, drone_position_y, side_length):
+    """
+    Given a physical position (x, y)[m] and the length of the square perimeter 
+    where simulation has to be ran, returns the four extremes of the perimeter (x, y)[m]
+    """
     half_side = side_length / 2.0
 
     drone_position_x = floor(drone_position_x)
@@ -12,13 +16,3 @@ def calculate_square_boundaries(drone_position_x, drone_position_y, side_length)
     lower_right = (drone_position_x + half_side, drone_position_y - half_side)
 
     return [upper_left, upper_right, lower_left, lower_right]
-
-# def is_inside_square(x, y, boundaries):
-#     upper_left, upper_right, lower_left, lower_right = boundaries
-
-#     # Check if the coordinates are within the x and y boundaries
-#     if upper_left[0] <= x <= upper_right[0] and lower_left[0] <= x <= lower_right[0]:
-#         if lower_left[1] <= y <= upper_left[1] and lower_right[1] <= y <= upper_right[1]:
-#             return True
-
-#     return False
